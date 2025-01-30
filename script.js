1168df520a7b15b3d0efaeb73cf7968a4dd90381
Kai1den <Gautambairwa6891@gmail.com>
2025-01-30 19:15:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const nav = document.querySelector("nav ul");
-    const toggleBtn = document.createElement("div");
-    toggleBtn.innerHTML = "&#9776;";
-    toggleBtn.classList.add("toggle-menu");
-
-    document.querySelector("header").prepend(toggleBtn);
-
-    toggleBtn.addEventListener("click", function () {
-        nav.classList.toggle("show");
-    });
+    console.log("Portfolio Loaded Successfully!");
 });
