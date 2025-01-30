e3aabfda81d28a350b5bb51daf34d43e40ab306c
Kai1den <Gautambairwa6891@gmail.com>
2025-01-30 17:38:58 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-// Example of a simple smooth scroll effect for the portfolio navigation
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
-
         document.querySelector(this.getAttribute('href')).scrollIntoView({
             behavior: 'smooth'
         });
