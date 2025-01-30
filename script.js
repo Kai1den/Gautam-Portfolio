323a0dac661dbb59cd844c4b61f40c69e137cc1b
Kai1den <Gautambairwa6891@gmail.com>
2025-01-30 18:05:33 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const navLinks = document.querySelectorAll("nav ul li a");
-
-    navLinks.forEach(link => {
-        link.addEventListener("click", function(event) {
-            event.preventDefault();
-            const targetSection = document.querySelector(this.getAttribute("href"));
-            if (targetSection) {
-                window.scrollTo({
-                    top: targetSection.offsetTop - 60,
-                    behavior: "smooth"
-                });
-            }
-        });
-    });
-
-    console.log("Portfolio Loaded Successfully!");
+    console.log("Portfolio website loaded successfully!");
 });
